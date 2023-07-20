1b1baeec8a72314cc392a9428cb76b79f0eb3a89
brandon-schabel <brandonschabel1995@gmail.com>
2023-07-19 19:06:17 -0700

    upates to sqlite factory

--- modules/sqlite-factory/create-sqlite-factory.ts
+++ modules/sqlite-factory/create-sqlite-factory.ts
@@ -1,10 +1,11 @@
 import { Database } from "bun:sqlite";
+import { SchemaType, TypeInference, TypeMapping } from "types";
 import { createValidatorFactory } from "../validation-factory";
 
-export function createTableQuery<Schema extends object>(
-  tableName: string,
-  schema: Schema
-): string {
+// Utility functions
+export function createTableQuery<
+  Schema extends Record<string, keyof TypeMapping>
+>(tableName: string, schema: Schema): string {
   const fields = Object.entries(schema)
     .map(([key, type]) => `${key} ${type.toUpperCase()}`)
     .join(", ");
@@ -12,84 +13,84 @@ export function createTableQuery<Schema extends object>(
   return `CREATE TABLE IF NOT EXISTS ${tableName} (${fields});`;
 }
 
-export type CreateSqliteInterface<Schema extends object> = {
-  create: (item: Schema) => Promise<void>;
-  read: () => Promise<Schema[]>;
-  update: (id: number, item: Partial<Schema>) => Promise<void>;
+export type CreateSqliteInterface<Schema extends SchemaType> = {
+  create: (item: TypeInference<Schema>) => Promise<void>;
+  read: () => Promise<TypeInference<Schema>[]>;
+  update: (id: number, item: Partial<TypeInference<Schema>>) => Promise<void>;
   deleteById: (id: number) => Promise<void>;
 };
 
-export function createSqliteFactory(db: Database) {
-  function createSqliteTableFactory<Schema extends object>(
-    tableName: string,
-    schema: Schema
-  ): CreateSqliteInterface<Schema> {
-    const { validateItem, validateAgainstArraySchema } =
-      createValidatorFactory(schema);
+// CRUD Interface
+export function createSqliteFactory<
+  Schema extends Record<string, keyof TypeMapping>
+>(db: Database, tableName: string, schema: Schema) {
+  const { validateAgainstArraySchema } = createValidatorFactory(schema);
 
-    // Create table
-    const createTable = db.query(createTableQuery(tableName, schema));
-    createTable.run();
+  // Create table
+  const createTable = db.query(createTableQuery(tableName, schema));
+  createTable.run();
 
-    // Create
-    async function create(item: Schema) {
-      const placeholders = Object.keys(schema)
-        .map((key) => `$${key}`)
-        .join(", ");
-      const insertQuery = db.query(
-        `INSERT INTO ${tableName} (${Object.keys(schema).join(
-          ", "
-        )}) VALUES (${placeholders});`
-      );
+  // Create
+  async function create(item: Schema) {
+    const placeholders = Object.keys(schema)
+      .map((key) => `$${key}`)
+      .join(", ");
+    const insertQuery = db.query(
+      `INSERT INTO ${tableName} (${Object.keys(schema).join(
+        ", "
+      )}) VALUES (${placeholders});`
+    );
 
-      // Run the query with the item object
-      insertQuery.run(item as any);
+    // Run the query with the item object
+    insertQuery.run(item as any);
+  }
+
+  // Read
+  async function read(): Promise<Schema[]> {
+    const selectQuery = db.query(`SELECT * FROM ${tableName};`);
+    const data = selectQuery.all();
+    const { error, data: validatedData } = validateAgainstArraySchema(
+      schema,
+      data
+    );
+
+    if (error) {
+      throw new Error(`Error during read: ${error}`);
     }
 
-    // Read
-    async function read(): Promise<Schema[]> {
-      const selectQuery = db.query(`SELECT * FROM ${tableName};`);
-      const data = selectQuery.all();
-      const { error, data: validatedData } = validateAgainstArraySchema(
-        schema,
-        data
-      );
-
-      if (error) {
-        throw new Error(`Error during read: ${error}`);
-      }
-
-      return validatedData!;
+    if (!validatedData) {
+      throw new Error(`Error during read, no data found: ${error}`);
     }
 
-    // Update
-    async function update(id: number, item: Partial<Schema>): Promise<void> {
-      const updateFields = Object.keys(item)
-        .map((key) => `${key} = $${key}`)
-        .join(", ");
-      const updateQuery = db.query(
-        `UPDATE ${tableName} SET ${updateFields} WHERE id = $id;`
-      );
+    return validatedData;
+  }
 
-      // Run the query with the item object and id
-      updateQuery.run({ ...item, id } as any);
-    }
+  // Update
+  async function update(
+    id: number,
+    item: Partial<TypeInference<Schema>>
+  ): Promise<void> {
+    const updateFields = Object.keys(item)
+      .map((key) => `${key} = $${key}`)
+      .join(", ");
+    const updateQuery = db.query(
+      `UPDATE ${tableName} SET ${updateFields} WHERE id = $id;`
+    );
 
-    // Delete
-    async function deleteById(id: number): Promise<void> {
-      const deleteQuery = db.query(`DELETE FROM ${tableName} WHERE id = $id;`);
-      deleteQuery.run({ id });
-    }
+    // Run the query with the item object and id
+    updateQuery.run({ ...item, id } as any);
+  }
 
-    return {
-      create,
-      read,
-      update,
-      deleteById,
-    };
+  // Delete
+  async function deleteById(id: number): Promise<void> {
+    const deleteQuery = db.query(`DELETE FROM ${tableName} WHERE id = $id;`);
+    deleteQuery.run({ id });
   }
 
   return {
-    createSqliteTableFactory,
+    create,
+    read,
+    update,
+    deleteById,
   };
 }
