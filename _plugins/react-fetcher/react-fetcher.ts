d8521652ad3d0c23ceb696db7f880c6f28918ba9
brandon-schabel <brandonschabel1995@gmail.com>
2023-08-05 19:51:23 -0700

    updated react-fetcher

--- _plugins/react-fetcher/react-fetcher.ts
+++ _plugins/react-fetcher/react-fetcher.ts
@@ -1,102 +1,157 @@
-import { createFetchFactory } from "instant-bun/modules/fetch-factory";
+import {
+  createFetchFactory,
+  FetchFactoryType,
+  HTTPMethod,
+} from "instant-bun/modules/fetch-factory/create-fetch-factory";
 import { useCallback, useMemo, useState } from "react";
 
-type FetchFactoryParams = Parameters<typeof createFetchFactory>[0];
+type RequestStatus = "idle" | "loading" | "success" | "error" | "no data";
 
-export function useFetcher<ResponseData>({
-  options,
-  fetchFactory,
-}: {
-  options?: FetchFactoryParams;
-  fetchFactory?: ReturnType<typeof createFetchFactory>;
-} = {}) {
-  if (!fetchFactory && !options) {
-    throw new Error("fetchFactory and options are required");
-  }
+export type FetcherConfig<
+  ResponseType,
+  BodyType = undefined,
+  ParamsType = undefined
+> = {
+  body?: BodyType;
+  params?: ParamsType;
+  response: ResponseType;
+};
 
-  const fetcher = useMemo(
-    () =>
-      fetchFactory ? fetchFactory : createFetchFactory(options ? options : {}),
-    [options]
+export type FetcherState<ResponseType> = {
+  data: ResponseType | null;
+  lastUpdated: number | null;
+  status: RequestStatus;
+  error: Error | null;
+};
+
+export type FetcherReturnType<DataType extends FetcherConfig<any, any, any>> = {
+  data: DataType["response"] | null;
+  lastUpdated: number | null;
+  status: RequestStatus;
+  error: Error | null;
+  get: (params?: DataType["params"]) => Promise<void>;
+  post: (data?: DataType["body"], params?: DataType["params"]) => Promise<void>;
+};
+
+export type Fetchers<
+  DataTypeMap extends Record<string, FetcherConfig<any, any, any>>
+> = {
+  [K in keyof DataTypeMap]: () => FetcherReturnType<DataTypeMap[K]>;
+};
+
+export type EndpointConfig<
+  DataTypeMap extends Record<string, FetcherConfig<any, any, any>>,
+  K extends keyof DataTypeMap
+> = {
+  endpoint: K;
+  method: HTTPMethod;
+  body?: DataTypeMap[K]["body"];
+  params?: DataTypeMap[K]["params"];
+};
+
+export type FetchFactoryConfig<
+  DataTypeMap extends Record<string, FetcherConfig<any, any, any>>
+> = {
+  baseUrl?: string;
+  logMode?: boolean;
+  endpoints: Array<EndpointConfig<DataTypeMap, keyof DataTypeMap>>;
+  defaultHeaders?: Record<string, string>;
+};
+
+export function createFetcher<DataType extends FetcherConfig<any, any, any>>(
+  fetchFactory: FetchFactoryType<DataType>,
+  endpointConfig: EndpointConfig<any, keyof DataType>,
+  defaultHeaders?: Record<string, string>
+): () => FetcherReturnType<DataType> {
+  const [state, setState] = useState<FetcherState<DataType["response"]>>({
+    data: null,
+    lastUpdated: null,
+    status: "idle",
+    error: null,
+  });
+
+  // TODO implement abort controller
+  // const abortController = new AbortController();
+
+  const fetchData = useCallback(
+    async (
+      method: HTTPMethod,
+      body?: DataType["body"],
+      params?: DataType["params"]
+    ) => {
+      setState((prevState) => ({ ...prevState, status: "loading" }));
+      try {
+        let result;
+        switch (method) {
+          case "get":
+            result = await fetchFactory.get(
+              endpointConfig.endpoint as string,
+              endpointConfig.params || params
+            );
+            break;
+          case "post":
+            result = await fetchFactory.post({
+              endpoint: endpointConfig.endpoint as string,
+              postData: endpointConfig.body || body,
+              params: endpointConfig.params || params,
+              headers: defaultHeaders,
+            });
+            break;
+          default:
+            throw new Error(`Unsupported method: ${method}`);
+        }
+        setState({
+          data: result.data,
+          lastUpdated: Date.now(),
+          status: "success",
+          error: null,
+        });
+      } catch (error) {
+        setState({
+          data: null,
+          lastUpdated: null,
+          status: "error",
+          error: error as Error,
+        });
+      }
+    },
+    [fetchFactory, endpointConfig, defaultHeaders]
   );
 
-  const [data, setData] = useState<ResponseData | null>(null);
-  // last updated is a unix timestamp of the last time the data was received
-  const [lastUpdated, setLastedUpdate] = useState<number | null>();
-  const [status, setStatus] = useState<
-    "idle" | "loading" | "success" | "error" | "no data"
-  >("idle");
+  const get = useCallback(
+    (params?: DataType["params"]) => fetchData("get", undefined, params),
+    [fetchData]
+  );
 
-  async function get(endpoint: string): Promise<void> {
-    setStatus("loading");
-    try {
-      const result = await fetcher.get<ResponseData>(endpoint);
-      setData(result.data);
-      setLastedUpdate(Date.now());
-      setStatus("success");
-    } catch (error) {
-      setStatus("error");
-      throw error;
-    }
-  }
+  const post = useCallback(
+    (data?: DataType["body"], params?: DataType["params"]) =>
+      fetchData("post", data, params),
+    [fetchData]
+  );
 
-  async function post<PostData extends object, Params extends object>({
-    data,
-    endpoint,
-    params,
-  }: {
-    endpoint: string;
-    data: PostData;
-    params: Params;
-  }): Promise<void> {
-    setStatus("loading");
-    try {
-      const result = await fetcher.postJson<ResponseData>({
-        postData: data,
-        endpoint,
-        params,
-      });
-      setData(result?.data || null);
-      setLastedUpdate(Date.now());
-      setStatus("success");
-    } catch (error) {
-      setStatus("error");
-      throw error;
-    }
-  }
+  return () => ({
+    ...state,
+    get,
+    post,
+  });
+}
 
-  async function postJSON<PostData>(
-    endpoint: string,
-    data: PostData
-  ): Promise<void> {
-    setStatus("loading");
-    try {
-      const result = await fetcher.postJson<ResponseData>({
-        endpoint,
-        postData: data,
-      });
-      if (result.data) {
-        setData(result?.data || null);
-        setLastedUpdate(Date.now());
-        setStatus("success");
-      } else {
-        setStatus("no data");
-      }
-    } catch (error) {
-      setStatus("error");
-      throw error;
-    }
-  }
+export function useApiFactory<
+  DataTypeMap extends Record<string, FetcherConfig<any, any, any>>
+>(config: FetchFactoryConfig<DataTypeMap>): Fetchers<DataTypeMap> {
+  const fetchFactory = useMemo(() => createFetchFactory(config), [config]);
 
-  const getData = useCallback(() => {
-    return data;
-  }, [data]);
+  const fetchers = useMemo(() => {
+    const fetcherFns: Partial<Fetchers<DataTypeMap>> = {};
+    config.endpoints.forEach((endpointConfig) => {
+      fetcherFns[endpointConfig.endpoint] = createFetcher(
+        fetchFactory,
+        endpointConfig,
+        config.defaultHeaders
+      );
+    });
+    return fetcherFns as Fetchers<DataTypeMap>;
+  }, [config, fetchFactory]);
 
-  const useData = () => {
-    return useMemo(() => {
-      return getData();
-    }, [lastUpdated]);
-  };
-
-  return { get, post, getData, status, lastUpdated, useData, postJSON };
+  return fetchers;
 }
