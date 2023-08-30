c8ff4c33aa69a79367132f2ae59043e4a5195424
brandon-schabel <brandonschabel1995@gmail.com>
2023-08-30 11:45:53 -0700

    updated alpha publish script

--- modules/npm-release/npm-release.ts
+++ modules/npm-release/npm-release.ts
@@ -23,7 +23,7 @@ export function npmReleaseFactory(options: NpmReleaseFactoryOptions) {
       if (!options.npmToken) {
         console.error("NPM_TOKEN is not set in environment variables.");
         exit(1);
-      } 
+      }
       const npmrcContent = `//registry.npmjs.org/:_authToken=${npmToken}`;
       const npmrcPath = path.resolve(process.cwd(), ".npmrc");
       Bun.write(npmrcPath, npmrcContent);
@@ -48,7 +48,13 @@ export function npmReleaseFactory(options: NpmReleaseFactoryOptions) {
         `Publishing from directory: ${dir}, attempt ${i + 1} of ${maxRetries}`
       );
 
-      const proc = Bun.spawnSync(["npm", "publish"], {
+      const publishScript = ["npm", "publish"];
+
+      if (isAlpha) {
+        publishScript.push("--tag", "alpha");
+      }
+
+      const proc = Bun.spawnSync(publishScript, {
         cwd: dir,
       });
 
