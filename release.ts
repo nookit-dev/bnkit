de91eca81740411bfb694d4f9e2520ba06103596
brandon-schabel <brandonschabel1995@gmail.com>
2023-08-29 19:27:14 -0700

    updated release script

--- release.ts
+++ release.ts
@@ -5,6 +5,11 @@ import { exit } from "process";
 const GITHUB_PAT = Bun.env.PERSONAL_ACCESS_TOKEN_GITHUB || "";
 const NPM_TOKEN = Bun.env.NPM_TOKEN || "";
 
+const ulog = (...args: any[]) => {
+  const currentTime = Bun.nanoseconds() / 1e9; // Convert to seconds
+  console.log(`[${currentTime.toFixed(3)}s]`, args);
+};
+
 /* Helper Functions */
 const setupNpmAuth = () => {
   try {
@@ -30,7 +35,7 @@ const npmPublish = async (packagePath: string, isAlpha: boolean) => {
   }
 
   try {
-    console.log(`Publishing from directory: ${dir}`);
+    ulog(`Publishing from directory: ${dir}`);
     await Bun.spawn(npmArgs, { cwd: dir });
   } catch (error) {
     console.error(`Failed to publish from ${dir}:`, error);
@@ -49,17 +54,19 @@ const updateVersion = (currentVersion: string, isAlpha: boolean): string => {
 };
 
 const updatePackageVersion = async (packagePath: string, isAlpha: boolean) => {
-  console.log(`Updating ${packagePath}`);
+  ulog(`Updating ${packagePath}`);
   const packageData = await Bun.file(packagePath).text();
   const parsedData = JSON.parse(packageData);
   parsedData.version = updateVersion(parsedData.version, isAlpha);
   await Bun.write(packagePath, JSON.stringify(parsedData, null, 2));
 };
 
-const gitCmd = async (commands: string[]) => {
+const gitCmd = async (commands: string[], log = true) => {
   const commandArray = ["git", ...commands];
 
   try {
+    if (log) ulog(`Running command: ${commandArray}`);
+
     await Bun.spawn(commandArray);
   } catch (error) {
     console.error(`Failed to run command: ${commandArray}:`, error);
@@ -68,7 +75,7 @@ const gitCmd = async (commands: string[]) => {
 };
 
 const commitAndPush = async () => {
-  console.log("*** Running git commands ***");
+  ulog("*** Running git commands ***");
 
   // Use the PAT to set the remote URL with authentication
   await gitCmd([
@@ -78,14 +85,44 @@ const commitAndPush = async () => {
     `https://${GITHUB_PAT}@github.com/brandon-schabel/u-tools.git`,
   ]);
 
-  console.log("Configured GitHub User");
+  ulog("Configured GitHub User");
   await gitCmd(["config", "user.name"]);
-  console.log("Configured GitHub Email");
+  ulog("Configured GitHub Email");
   await gitCmd(["config", "user.email"]);
 
   await gitCmd(["add", "."]);
   await gitCmd(["commit", "-m", "Bump versions"]);
   await gitCmd(["push", "origin", "HEAD:main"]);
+};
+
+const setupGitConfig = async () => {
+  ulog("*** Configuring Git ***");
+
+  // Configure user name and email
+  ulog("Configuring GitHub User");
+  await gitCmd(["config", "--global", "user.name", "github-actions[bot]"]);
+
+  ulog("Configured Git User: ", await gitCmd(["config", "user.name"]));
+
+  ulog("Configuring GitHub Email");
+  await gitCmd([
+    "config",
+    "--global",
+    "user.email",
+    "41898282+github-actions[bot]@users.noreply.github.com",
+  ]);
+
+  ulog("Configured Git Email: ", await gitCmd(["config", "user.email"]));
+
+  // Use the PAT to set the remote URL with authentication
+  ulog("Setting up remote URL with PAT");
+
+  await gitCmd([
+    "remote",
+    "set-url",
+    "origin",
+    `https://${GITHUB_PAT}@github.com/brandon-schabel/u-tools.git`,
+  ]);
 };
 
 const isLocalRun = process.env.LOCAL_RUN === "true";
@@ -106,9 +143,10 @@ const pluginReactPath = path.resolve(
 /* Script */
 if (!isLocalRun) {
   setupNpmAuth();
+  await setupGitConfig();
 }
 
-console.log(`Updating versions to ${isAlpha ? "alpha" : "Release"}`);
+ulog(`Updating versions to ${isAlpha ? "alpha" : "Release"}`);
 await updatePackageVersion(corePackagePath, isAlpha);
 await npmPublish(corePackagePath, isAlpha);
 
