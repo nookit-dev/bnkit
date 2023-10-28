094e6562520f03160c0f4669fcc2a09cb590a574
brandon-schabel <brandonschabel1995@gmail.com>
2023-10-27 19:34:57 -0700

    turn hash factory into auth factory

---
+++ modules/auth/hash-factory.test.ts
@@ -0,0 +1,53 @@
+import { describe, expect, test } from "bun:test";
+import { createNonSecureHashFactory } from "./hash-factory";
+
+describe("NonSecureHashFactory", () => {
+  const factory = createNonSecureHashFactory();
+
+  describe("hash function", () => {
+    test("should hash string input", () => {
+      const input = "test";
+      const hashedValue = factory.hash(input);
+
+      expect(typeof hashedValue).toBe("bigint"); // assuming wyhash returns bigint
+    });
+
+    test("should hash ArrayBuffer input", () => {
+      const buffer = new ArrayBuffer(8);
+      const hashedValue = factory.hash(buffer);
+
+      expect(typeof hashedValue).toBe("bigint");
+    });
+
+    test("should hash with a provided seed", () => {
+      const input = "test";
+      const seed = 1234;
+      const hashedValueWithSeed = factory.hash(input, seed);
+      const hashedValueWithoutSeed = factory.hash(input);
+
+      expect(hashedValueWithSeed).not.toEqual(hashedValueWithoutSeed);
+    });
+  });
+
+  describe("hashWithAlgorithm function", () => {
+    test.each([
+      ["wyhash", "test"],
+      ["crc32", new ArrayBuffer(8)],
+      ["adler32", "another test"],
+      //... add other algorithms and data samples
+    ])(
+      "should hash using the %s algorithm with provided data",
+      (algorithm, data) => {
+        const hashedValue = factory.hashWithAlgorithm(algorithm, data);
+
+        expect(hashedValue).toBeDefined();
+      }
+    );
+
+    test("should throw error for an unsupported algorithm", () => {
+      expect(() => {
+        factory.hashWithAlgorithm("unsupportedAlgorithm", "test");
+      }).toThrow();
+    });
+  });
+});
---
+++ modules/auth/index.ts
@@ -0,0 +1,9 @@
+export { createNonSecureHashFactory } from "./hash-factory";
+export { createSecureHashFactory } from "./secure-hash-factory";
+
+export {
+  createSecurityToken,
+  createToken,
+  getTokenExpireEpoch,
+  verifyToken
+} from "./security-token";
