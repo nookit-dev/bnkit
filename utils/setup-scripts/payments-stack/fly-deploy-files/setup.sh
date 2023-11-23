6dfcc8ef7f8007e5f7dae7534a1d916942d9987c
brandon-schabel <brandonschabel1995@gmail.com>
2023-11-23 12:29:30 -0700

    updated payments stack deploy script

--- utils/setup-scripts/payments-stack/fly-deploy-files/setup.sh
+++ utils/setup-scripts/payments-stack/fly-deploy-files/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the repository URL
-REPO_URL="https://raw.githubusercontent.com/brandon-schabel/bun-nook-kit/main/utils/setup-scripts/payments-stack"
+REPO_URL="https://raw.githubusercontent.com/brandon-schabel/bun-nook-kit/main/utils/setup-scripts/payments-stack/fly-deploy-files"
 
 # Download the files
 curl -O "$REPO_URL/.dockerignore"
@@ -9,13 +9,13 @@ curl -O "$REPO_URL/deploy-to-fly.yml"
 curl -O "$REPO_URL/Dockerfile"
 curl -O "$REPO_URL/fly.toml"
 
-# Move .dockerignore, Dockerfile, and fly.toml to the current working directory (project root)
-mv .dockerignore .
-mv Dockerfile .
-mv fly.toml .
-
 # Move deploy-to-fly.yml to .github/workflows in the current working directory
 mkdir -p .github/workflows
 mv deploy-to-fly.yml .github/workflows/
 
 echo "Files have been moved successfully."
+
+echo "If you don't already have fly installed"
+echo "install with: brew install flyctl"
+echo "or with curl/linux: curl -L https://fly.io/install.sh | sh"
+echo "Now that you are setup with fly, you can delete this file."
