e2093d2483e01277daf54afce36aa7c688769aee
brandon-schabel <brandonschabel1995@gmail.com>
2023-11-23 14:04:57 -0700

    clean up fly setup script

--- utils/setup-scripts/fly-deploy-files/setup.sh
+++ utils/setup-scripts/fly-deploy-files/setup.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-# Define the repository URL
-REPO_URL="https://raw.githubusercontent.com/brandon-schabel/bun-nook-kit/main/utils/setup-scripts/payments-stack/fly-deploy-files"
 
-# Download the files
-curl -O "$REPO_URL/.dockerignore"
-curl -O "$REPO_URL/deploy-to-fly.yml"
-curl -O "$REPO_URL/Dockerfile"
-curl -O "$REPO_URL/fly.toml"
-curl -O "$REPO_URL/fly-readme.md"
+BNK_SCRIPT_PATH="node_modules/bnkit/utils/setup-scripts/fly-deploy-files"
+
+# parse all flags 
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+    fi
+    shift
+done
+
+# getting dir flag
+PROJECT_DIR=$dir
+FULL_PATH="$PROJECT_DIR/$BNK_SCRIPT_PATH"
+
+cp -r $FULL_PATH/.dockerignore $PROJECT_DIR/.dockerignore
+cp -r $FULL_PATH/deploy-to-fly.yml PROJECT_DIR/.github/workflows
+cp -r $FULL_PATH/Dockerfile $PROJECT_DIR/Dockerfile
+cp -r $FULL_PATH/fly.toml $PROJECT_DIR/fly.toml
+
 
 # Move deploy-to-fly.yml to .github/workflows in the current working directory
-mkdir -p .github/workflows
-mv deploy-to-fly.yml .github/workflows/
+mkdir -p $PROJECT_DIR/.github/workflows
+cp -r $FULL_PATH/deploy-to-fly.yml $PROJECT_DIR/.github/workflows/deploy-to-fly.yml
 
 echo "Files have been moved successfully."
 
